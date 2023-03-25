4be96d4b487b833a7c99d82525477e5be2b71825
Ferris Lucas <ferris.lucas@gmail.com>
2023-03-24 19:47:59 -0500

    wip

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+Main.call()
+
 import RefactorService from './refactorService.js'
 import readline from 'readline'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 
-export default class {
+class Main {
   static async call() {
     const args = process.argv.slice(2)
     const lastArgument = args.slice(-1)[0]
@@ -23,33 +25,44 @@ export default class {
     const preamble = await RefactorService.load(path.join(__dirname, 'preamble.txt'))
 
     while (true) {
-      let userInput = await new Promise(resolve => {
-        rl.question('promptr# ', _input => {
-          resolve(_input)
-        })
-      })
+      let userInput = await Main.getUserInput(rl);
       if (!userInput) continue
       if (userInput == 'exit' || userInput == "\\q") break
 
-      let prompt = userInput.toString().trim();
-
-      let additionalContext = `Here are some unit tests that you need to make pass:\n\n`;
-      for (let n = 0; n < argsExceptLast.length; n++) {
-        const filename = argsExceptLast[n]
-        let s = await RefactorService.load(path.join(process.cwd(), filename))
-        additionalContext = additionalContext.concat(
-          `Unit tests in file called "${filename}":\n${s}\n------------------\n\n`
-        )
-      }
-      let context = await RefactorService.load(outputFile)
-      if (context?.trim()?.length > 0) {
-        prompt = `${preamble} ${
-          additionalContext.length > 0 ? additionalContext : ''
-        } Your instruction is: ${prompt} \n The current source code is: ${context}`;
-      }
-      //console.log(`Prompt: \n${prompt}\n\n`)
-      await RefactorService.call(prompt, outputFile, true)
+      await Main.plugin(userInput, argsExceptLast, outputFile, preamble)
     }
     rl.close()
+  }
+
+  static async plugin(userInput, argsExceptLast, outputFile, preamble) {
+    let prompt = userInput.toString().trim()
+
+    let additionalContext = await Main.getAdditionalContext(argsExceptLast)
+    let context = await RefactorService.load(outputFile)
+    if (context?.trim()?.length > 0) {
+      prompt = `${preamble} ${additionalContext.length > 0 ? additionalContext : ''} Your instruction is: ${prompt} \n The current source code is: ${context}`
+    }
+    //console.log(`Prompt: \n${prompt}\n\n`)
+    await RefactorService.call(prompt, outputFile, true)
+  }
+
+  static async getUserInput(rl) {
+    return new Promise(resolve => {
+      rl.question('promptr# ', _input => {
+        resolve(_input)
+      })
+    })
+  }
+
+  static async getAdditionalContext(argsExceptLast) {
+    let additionalContext = `Here are some unit tests that you need to make pass:\n\n`;
+    for (let n = 0; n < argsExceptLast.length; n++) {
+      const filename = argsExceptLast[n]
+      let s = await RefactorService.load(path.join(process.cwd(), filename))
+      additionalContext = additionalContext.concat(
+        `Unit tests in file called "${filename}":\n${s}\n------------------\n\n`
+      )
+    }
+    return additionalContext;
   }
 }
