a8277099fafccbcabb5b5c61c065ff931b750c62
Ferris Lucas <ferris.lucas@gmail.com>
2023-03-24 20:37:08 -0500

    wip

--- main.js
+++ main.js
@@ -1,48 +1,24 @@
-import RefactorService from './refactorService.js'
 import readline from 'readline'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import PluginService from './pluginService.js'
 
 export default class Main {
   
   static async call() {
     const args = process.argv.slice(2)
-    const lastArgument = args.slice(-1)[0]
-    const outputFile = path.join(process.cwd(), lastArgument)
-    console.log(`Output file is: ${outputFile}`)
-
-    const argsExceptLast = args.slice(0, -1)
 
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     })
-  
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = dirname(__filename)
-    const preamble = await RefactorService.load(path.join(__dirname, 'preamble.txt'))
 
     while (true) {
       let userInput = await this.getUserInput(rl);
       if (!userInput) continue
       if (userInput == 'exit' || userInput == "\\q") break
 
-      await this.plugin(userInput, argsExceptLast, outputFile, preamble)
+      await PluginService.plugin(userInput, args)
     }
     rl.close()
-  }
-
-  static async plugin(userInput, argsExceptLast, outputFile, preamble) {
-    let prompt = userInput.toString().trim()
-
-    let additionalContext = await this.getAdditionalContext(argsExceptLast)
-    let context = await RefactorService.load(outputFile)
-    if (context?.trim()?.length > 0) {
-      prompt = `${preamble} ${additionalContext.length > 0 ? additionalContext : ''} Your instruction is: ${prompt} \n The current source code is: ${context}`
-    }
-    //console.log(`Prompt: \n${prompt}\n\n`)
-    await RefactorService.call(prompt, outputFile)
   }
 
   static async getUserInput(rl) {
@@ -53,15 +29,4 @@ export default class Main {
     })
   }
 
-  static async getAdditionalContext(argsExceptLast) {
-    let additionalContext = `Here are some unit tests that you need to make pass:\n\n`;
-    for (let n = 0; n < argsExceptLast.length; n++) {
-      const filename = argsExceptLast[n]
-      let s = await RefactorService.load(path.join(process.cwd(), filename))
-      additionalContext = additionalContext.concat(
-        `Unit tests in file called "${filename}":\n${s}\n------------------\n\n`
-      )
-    }
-    return additionalContext;
-  }
 }
