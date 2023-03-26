ff1509002b8f9f1054b38c0d1d96a5729328ce70
Ferris Lucas <ferris.lucas@gmail.com>
2023-03-25 23:50:15 -0500

    Stop if no output file is specified

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ import PluginService from './pluginService.js'
 export default class Main {
   
   static async call() {
+    if (process.argv.length <= 2) {
+      console.log("Usage: promptr [<input path 1> <input path 2> ...] <output path>");
+      process.exit(-1);
+    }
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
