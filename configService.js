dcac00c7dda21f5486600ad135ee917ac890fc91
Ferris Lucas <ferris.lucas@gmail.com>
2023-03-27 21:53:17 -0500

    cleanup

--- configService.js
+++ configService.js
@@ -1,7 +1,7 @@
 import { FileService } from "./fileService.js";
 
 export default class ConfigService {
-static async retrieveConfig() {
+  static async retrieveConfig() {
     const config = { model: "text-davinci-003", temperature: 0.5 };
     const userHomeDir = process.env.HOME;
     const configPath = `${userHomeDir}/.promptr.json`;
