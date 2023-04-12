10e6baddb0fb26bbcc647219aa05c539bb5c6241
Ferris Lucas <ferris.lucas@gmail.com>
2023-04-12 14:05:55 -0500

    cleanup

--- refactorResultProcessor.js
+++ refactorResultProcessor.js
@@ -4,17 +4,16 @@ import path from 'path';
 export default class RefactorResultProcessor {
   static call(data) {
     data.operations.forEach((operation) => {
-      const filePath = operation.filePath;
-      const fileContents = operation.fileContents;
-      const absolutePath = path.resolve(filePath);
-      const dirPath = path.dirname(absolutePath);
+      const filePath = operation.filePath
+      const fileContents = operation.fileContents
+      const absolutePath = path.resolve(filePath)
+      const dirPath = path.dirname(absolutePath)
 
       if (operation.crudOperation === 'create' || operation.crudOperation === 'update') {
         if (!fs.existsSync(dirPath)) {
-          fs.mkdirSync(dirPath, { recursive: true });
+          fs.mkdirSync(dirPath, { recursive: true })
         }
-
-        fs.writeFileSync(absolutePath, fileContents, 'utf-8');
+        fs.writeFileSync(absolutePath, fileContents, 'utf-8')
       } else if (operation.crudOperation === 'delete') {
         if (fs.existsSync(absolutePath)) {
           fs.unlinkSync(absolutePath);
@@ -27,9 +26,8 @@ export default class RefactorResultProcessor {
         if (!fs.existsSync(destinationDirPath)) {
           fs.mkdirSync(destinationDirPath, { recursive: true });
         }
-
         fs.renameSync(absolutePath, absoluteDestinationPath);
       }
-    });
+    })
   }
 }
