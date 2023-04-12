e0bc6766eb2df657d07a603e87aad31ebfbf5f7d
Ferris Lucas <ferris.lucas@gmail.com>
2023-04-12 14:25:22 -0500

    Implement "move" and "delete" CRUD operations (#19)
    * Support updates and deletes
    
    * Support move operations
    
    * cleanup
    
    * Handle deleting folders
    
    * cleanup

--- refactorResultProcessor.js
+++ refactorResultProcessor.js
@@ -4,16 +4,34 @@ import path from 'path';
 export default class RefactorResultProcessor {
   static call(data) {
     data.operations.forEach((operation) => {
-      const filePath = operation.filePath;
-      const fileContents = operation.fileContents;
-      const absolutePath = path.resolve(filePath);
-      const dirPath = path.dirname(absolutePath);
+      const filePath = operation.filePath
+      const fileContents = operation.fileContents
+      const absolutePath = path.resolve(filePath)
+      const dirPath = path.dirname(absolutePath)
 
-      if (!fs.existsSync(dirPath)) {
-        fs.mkdirSync(dirPath, { recursive: true });
+      if (operation.crudOperation === 'create' || operation.crudOperation === 'update') {
+        if (!fs.existsSync(dirPath)) {
+          fs.mkdirSync(dirPath, { recursive: true })
+        }
+        fs.writeFileSync(absolutePath, fileContents, 'utf-8')
+      } else if (operation.crudOperation === 'delete') {
+        if (fs.existsSync(absolutePath)) {
+          if (fs.lstatSync(absolutePath).isDirectory()) {
+            fs.rmdirSync(absolutePath, { recursive: true });
+          } else {
+            fs.unlinkSync(absolutePath);
+          }
+        }
+      } else if (operation.crudOperation === 'move') {
+        const destinationPath = operation.destinationPath;
+        const absoluteDestinationPath = path.resolve(destinationPath);
+        const destinationDirPath = path.dirname(absoluteDestinationPath);
+
+        if (!fs.existsSync(destinationDirPath)) {
+          fs.mkdirSync(destinationDirPath, { recursive: true });
+        }
+        fs.renameSync(absolutePath, absoluteDestinationPath);
       }
-
-      fs.writeFileSync(absolutePath, fileContents, 'utf-8');
-    });
+    })
   }
 }
