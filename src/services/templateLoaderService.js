1cd5335465f6b0c627d0881e8b144e903668065d
Ferris Lucas <ferris.lucas@gmail.com>
2023-04-14 16:55:20 -0500

    Retrieve built in templates from gists

--- src/services/templateLoaderService.js
+++ src/services/templateLoaderService.js
@@ -7,9 +7,20 @@ import { FileService } from './fileService.js'
 
 class TemplateLoader {
   static async loadTemplate(prompt, context, template) {
-    const templateText = template.startsWith("http://") || template.startsWith("https://") ? 
-      await this.loadTemplateFromUrl(template) : 
-      await this.loadTemplateFromPath(template)
+    let templateText;
+    if (template === 'refactor') {
+      templateText = await this.loadTemplateFromUrl('https://gist.githubusercontent.com/ferrislucas/0c9c054133660e4f744e80b685417cfd/raw/6a19ad3f2084c67af4edbcdeb79f2f050433ff67/promptr-refactor-template-v3.0.2');
+    } else if (template === 'empty') {
+      templateText = await this.loadTemplateFromUrl('https://gist.githubusercontent.com/ferrislucas/e43ce36b49f37efe28e7414de4b71399/raw/7ef0afd5a094d392c255d7c0a98f6572dfc4bece/promptr-empty-template-v3.0.2');
+    } else if (template === 'swe') {
+      templateText = await this.loadTemplateFromUrl('https://gist.githubusercontent.com/ferrislucas/a6a18fdafe32910c95829a700c0887ed/raw/50e533d2db8e7e138bfa925739e5e1f5c4498e95/promptr-swe-template-v3.0.2');
+    } else if (template === 'test-first') {
+      templateText = await this.loadTemplateFromUrl('https://gist.githubusercontent.com/ferrislucas/a6a18fdafe32910c95829a700c0887ed/raw/50e533d2db8e7e138bfa925739e5e1f5c4498e95/promptr-swe-template-v3.0.2');
+    } else {
+      templateText = template.startsWith('http://') || template.startsWith('https://') ? 
+        await this.loadTemplateFromUrl(template) : 
+        await this.loadTemplateFromPath(template);
+    }
     const engine = new Liquid()
     engine.registerFilter("jsonToObject", (json) => JSON.parse(json));
     const tpl = engine.parse(templateText)    
--- test/templateLoader.test.js
+++ test/templateLoader.test.js
@@ -10,7 +10,7 @@ import TemplateLoader from '../src/services/templateLoaderService.js';
 
 
 describe('TemplateLoader', () => {
-  describe("loadTemplateFromPath", () => {
+  describe('loadTemplateFromPath', () => {
     it('should call FileService.load with correct path when loadTemplateFromPath is passed the string "refactor"', async () => {
       const fileServiceLoadStub = sinon.stub(FileService, 'load').resolves('Test content');
 
@@ -20,9 +20,53 @@ describe('TemplateLoader', () => {
 
       fileServiceLoadStub.restore();
     });
+
+    describe("testing built in templates", () => {
+      let loadTemplateFromUrlStub
+
+      beforeEach(() => {
+        loadTemplateFromUrlStub = sinon.stub(TemplateLoader, 'loadTemplateFromUrl');
+      })
+
+      afterEach(() => {
+        loadTemplateFromUrlStub.restore()
+      })
+
+      it('should return correct content for the refactor template', async () => {
+        loadTemplateFromUrlStub.withArgs('https://gist.githubusercontent.com/ferrislucas/0c9c054133660e4f744e80b685417cfd/raw/6a19ad3f2084c67af4edbcdeb79f2f050433ff67/promptr-refactor-template-v3.0.2').resolves('Refactor content');
+        
+        const refactorContent = await TemplateLoader.loadTemplate('prompt', 'context', 'refactor');
+        
+        assert.strictEqual(refactorContent, 'Refactor content');      
+      })
+
+      it('should return correct content for the empty template', async () => {        
+        loadTemplateFromUrlStub.withArgs('https://gist.githubusercontent.com/ferrislucas/e43ce36b49f37efe28e7414de4b71399/raw/7ef0afd5a094d392c255d7c0a98f6572dfc4bece/promptr-empty-template-v3.0.2').resolves('Empty content');
+        
+        const emptyContent = await TemplateLoader.loadTemplate('prompt', 'context', 'empty');
+        
+        assert.strictEqual(emptyContent, 'Empty content');
+      })
+      
+      it('should return correct content for the swe template', async () => {
+        loadTemplateFromUrlStub.withArgs('https://gist.githubusercontent.com/ferrislucas/a6a18fdafe32910c95829a700c0887ed/raw/50e533d2db8e7e138bfa925739e5e1f5c4498e95/promptr-swe-template-v3.0.2').resolves('SWE content')
+
+        const sweContent = await TemplateLoader.loadTemplate('prompt', 'context', 'swe')
+
+        assert.strictEqual(sweContent, 'SWE content');
+      })
+
+      it('should return correct content for the test-first template', async () => {
+        loadTemplateFromUrlStub.withArgs('https://gist.githubusercontent.com/ferrislucas/a6a18fdafe32910c95829a700c0887ed/raw/50e533d2db8e7e138bfa925739e5e1f5c4498e95/promptr-swe-template-v3.0.2').resolves('Test-First content');
+
+        const testFirstContent = await TemplateLoader.loadTemplate('prompt', 'context', 'test-first');
+
+        assert.strictEqual(testFirstContent, 'Test-First content');
+      })
+    })
   });
 
-  describe("loadTemplate", () => {
+  describe('loadTemplate', () => {
     it('should load template from URL when template starts with http:// or https://', async () => {
       const loadTemplateFromUrlStub = sinon.stub(TemplateLoader, 'loadTemplateFromUrl').resolves('Test content from URL');
 
@@ -34,9 +78,10 @@ describe('TemplateLoader', () => {
     });
 
     it('should load template from path when template does not start with http:// or https://', async () => {
-      const loadTemplateFromPathStub = sinon.stub(TemplateLoader, 'loadTemplateFromPath').resolves('Test content from path');
+      const loadTemplateFromPathStub = sinon.stub(TemplateLoader, 'loadTemplateFromPath')
+      loadTemplateFromPathStub.resolves('Test content from path');
 
-      await TemplateLoader.loadTemplate('prompt', 'context', 'refactor');
+      await TemplateLoader.loadTemplate('prompt', 'context', 'userTemplate');
 
       assert(loadTemplateFromPathStub.called);
 
@@ -44,7 +89,7 @@ describe('TemplateLoader', () => {
     });
   });
 
-  describe("loadTemplateFromUrl", () => {
+  describe('loadTemplateFromUrl', () => {
     it('should fetch content from URL when loadTemplateFromUrl is called', async () => {
       const fetchStub = sinon.stub(global, 'fetch').resolves({ text: () => 'Test content from URL' });
 
