53bb481f4e63b85006e65e86bcbed7cdf3a4db8c
Ferris Lucas <ferris.lucas@gmail.com>
2023-04-19 22:23:44 -0500

    This template is more gpt3 friendly

--- src/services/templateUrl.js
+++ src/services/templateUrl.js
@@ -1,5 +1,5 @@
 class TemplateUrl {
-  static refactor = 'https://gist.githubusercontent.com/ferrislucas/0c9c054133660e4f744e80b685417cfd/raw/6a19ad3f2084c67af4edbcdeb79f2f050433ff67/promptr-refactor-template-v3.0.2';
+  static refactor = 'https://gist.githubusercontent.com/ferrislucas/c6a89d2a37d7e3a0882f1f3ed1cab2a7/raw/6ff336a91768572c294c714378992e35a127b787/promptr-refactor-template-v3.0.3';
   static empty = 'https://gist.githubusercontent.com/ferrislucas/e43ce36b49f37efe28e7414de4b71399/raw/7ef0afd5a094d392c255d7c0a98f6572dfc4bece/promptr-empty-template-v3.0.2';
   static swe = 'https://gist.githubusercontent.com/ferrislucas/a6a18fdafe32910c95829a700c0887ed/raw/50e533d2db8e7e138bfa925739e5e1f5c4498e95/promptr-swe-template-v3.0.2';
   static testFirst = 'https://gist.githubusercontent.com/ferrislucas/5d38034e1eefaec0a3d32bdbca3a9ac6/raw/48f1a47d179f568cf1d1fa9271d5ad13fbdc3c85/promptr-test-first-template-v3.0.2';
