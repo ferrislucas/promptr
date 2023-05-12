f7ab5f33203add330b61f271ff6565f82a371c5a
Ferris Lucas <ferris.lucas@gmail.com>
2023-05-11 20:03:12 -0500

    Use the updated refactor template which adheres to recommended best practices
    See recommendations here: https://help.openai.com/en/articles/6654000-best-practices-for-prompt-engineering-with-openai-api

--- src/services/templateUrl.js
+++ src/services/templateUrl.js
@@ -1,5 +1,5 @@
 class TemplateUrl {
-  static refactor = 'https://gist.githubusercontent.com/ferrislucas/f474ea2d1438c9740b29281c79c162ba/raw/529f65f7ea03f15b7f43f0afacd1ee62116eefa7/promptr-refactor-template-v4.0.0';
+  static refactor = 'https://gist.githubusercontent.com/ferrislucas/ef8782f34d0e8744cde63f200bfa49fa/raw/6dada2bf330783dc82b3e181571408d8dc79fa7a/promptr-refactor-template-v4.0.1';
   static empty = 'https://gist.githubusercontent.com/ferrislucas/e43ce36b49f37efe28e7414de4b71399/raw/7ef0afd5a094d392c255d7c0a98f6572dfc4bece/promptr-empty-template-v3.0.2';
   static swe = 'https://gist.githubusercontent.com/ferrislucas/a6a18fdafe32910c95829a700c0887ed/raw/50e533d2db8e7e138bfa925739e5e1f5c4498e95/promptr-swe-template-v3.0.2';
   static testFirst = 'https://gist.githubusercontent.com/ferrislucas/5d38034e1eefaec0a3d32bdbca3a9ac6/raw/48f1a47d179f568cf1d1fa9271d5ad13fbdc3c85/promptr-test-first-template-v3.0.2';
