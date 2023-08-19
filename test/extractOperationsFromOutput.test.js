949eeb05fbedd5fa3e3644406e19ea2fbf75a9df
Ferris Lucas <ferris.lucas@gmail.com>
2023-08-18 22:29:57 -0500

    cleanup

--- test/extractOperationsFromOutput.test.js
+++ test/extractOperationsFromOutput.test.js
@@ -6,39 +6,39 @@ describe('extractOperationsFromOutput', () => {
     {
       input: '{"key": "value"}',
       expectedOutput: { key: 'value' },
-      description: 'Simple JSON object',
+      description: 'Simple JSON object'
     },
     {
       input: 'random text {"key": "value"} more text',
       expectedOutput: { key: 'value' },
-      description: 'JSON object with surrounding text',
+      description: 'JSON object with surrounding text'
     },
     {
       input: '[{"key": "value1"}, {"key": "value2"}]',
       expectedOutput: [{ key: 'value1' }, { key: 'value2' }],
-      description: 'Simple JSON array',
+      description: 'Simple JSON array'
     },
     {
       input: 'random text [{"key": "value1"}, {"key": "value2"}] more text',
       expectedOutput: [{ key: 'value1' }, { key: 'value2' }],
-      description: 'JSON array with surrounding text',
+      description: 'JSON array with surrounding text'
     },
     {
       input: 'this is not a JSON object or array',
       expectedOutput: null,
-      description: 'No JSON object or array in the input string',
+      description: 'No JSON object or array in the input string'
     },
     {
       input: '{"key": "value" broken JSON',
       expectedOutput: null,
-      description: 'Invalid JSON object',
-    },
-  ];
+      description: 'Invalid JSON object'
+    }
+  ]
 
   testCases.forEach((testCase) => {
     it(testCase.description, () => {
-      const result = extractOperationsFromOutput(testCase.input);
-      assert.deepStrictEqual(result, testCase.expectedOutput);
-    });
-  });
-});
+      const result = extractOperationsFromOutput(testCase.input)
+      assert.deepStrictEqual(result, testCase.expectedOutput)
+    })
+  })
+})
