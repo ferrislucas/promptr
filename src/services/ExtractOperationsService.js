237027452c928a8e54ca99f3a1dd8f6578132659
Ferris Lucas <ferris.lucas@gmail.com>
2023-08-18 23:23:15 -0500

    Sometimes the json returned by the model delimits fileContent with a triple double quote

--- src/services/ExtractOperationsService.js
+++ src/services/ExtractOperationsService.js
@@ -4,10 +4,18 @@ export class ExtractOperationsService {
       const parsedJSON = JSON.parse(input);
       return parsedJSON;
     } catch (error) {
+      return this.tryAgain(input)
+    }
+  }
+
+  static tryAgain(input) {
+    try {
+      // Replace triple double quotes with single double quotes
+      const correctedString = input.replace(/"{3}|'{3}/g, "\"")
+      return JSON.parse(correctedString)
+    } catch (error) {
       const jsonRegex = /({.*}|\[.*\])/s; // Regular expression to match a JSON object or array
       const match = input.match(jsonRegex);
-      console.log("There was an error parsing the model's output:")
-      console.log(input)
       if (match && match[1]) {
         console.log('Attemping to extract json:')
         console.log(match[1])
@@ -16,10 +24,12 @@ export class ExtractOperationsService {
           return parsed;
         } catch (error) {
           console.log(error)
-          return null; // Return null if there's an error during JSON parsing
+          return null
         }
       } else {
-        return null; // Return null if no JSON object or array is found in the input string
+        console.log("There was an error parsing the model's output:")
+        console.log(input)
+        return null
       }
     }
   }
